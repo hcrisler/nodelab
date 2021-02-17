ee2b83a98f5ce792e05cb9536b95c740208a61e9
Hannah Crisler <z@Macintosh-3.local>
2021-02-16 22:14:07 -0500

    files

---
+++ quotes.js
@@ -0,0 +1,6 @@
+let quotes = ["Yo, I'll tell you what I want, what I really, really want", 
+"So tell me what you want, what you really, really want", 
+"I wanna, (ha) I wanna, (ha) I wanna, (ha) I wanna, (ha)",
+"I wanna really, really, really wanna party on March 25th"]
+
+module.exports = quotes;
